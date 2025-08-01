86092acc826982508993c5745de8c267428b2ad9
Hanaasagi <ambiguous404@gmail.com>
2025-08-01 15:35:31 +0900

    chore: improve banner alignment in test script

--- tools/manual-run-fixtures.sh
+++ tools/manual-run-fixtures.sh
@@ -2,12 +2,26 @@
 
 set -euo pipefail
 
+LINE_WIDTH=$(tput cols)
+LINE_WIDTH=$((LINE_WIDTH > 80 ? 80 : LINE_WIDTH))
+
+print_banner() {
+  local label="$1"
+  local total_len=${#label}
+  local prefix_len=$(((LINE_WIDTH - total_len - 2) / 2))
+  local suffix_len=$((LINE_WIDTH - total_len - 2 - prefix_len))
+  printf "%0.s=" $(seq 1 $prefix_len)
+  printf " %s " "$label"
+  printf "%0.s=" $(seq 1 $suffix_len)
+  echo
+}
+
 for file in test/e2e/fixtures/*.txt; do
-  echo "====================[ Running: $file ]===================="
+  label="Running: $file"
+  print_banner "$label"
   echo
   cat "$file" | ./build/magonote
   echo
-  echo "====================[ End of $file ]======================"
+  print_banner "End of $file"
   sleep 0.2
 done
-
